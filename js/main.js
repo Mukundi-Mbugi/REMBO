f5531312648f4120294541faa564cf3e9fabb4bb
Elijah Mbugi <mukundielijah@gmail.com>
2022-04-22 00:25:14 +0300

    Add form and price links

--- js/main.js
+++ js/main.js
@@ -1,53 +1,57 @@
-const findButton=document.getElementById('findbtn')
+const findButton = document.getElementById("findbtn");
 
-    findButton.addEventListener('click',(event)=>{
-      event.preventDefault();
-      const brand=document.getElementById('brands').value
-      console.log(brand)
-      fetch(`https://makeup-api.herokuapp.com/api/v1/products.json?brand=${brand}`)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
+findButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  const brand = document.getElementById("brands").value;
+  console.log(brand);
+  fetch(`https://makeup-api.herokuapp.com/api/v1/products.json?brand=${brand}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
 
-    data.forEach((product)=>{
-      const parentDiv=document.getElementById('item')
-      const productDiv= document.createElement('div')
-      const titleElement= document.createElement('h6')
-      const imageElement= document.createElement('img')
-      const descriptionElement= document.createElement('p')
-      const priceElement= document.createElement('h5')
-      const priceButton = document.createElement('button')
-  
-  
-  
-      titleElement.innerText = product.name;
-      productDiv.appendChild(titleElement)
-      parentDiv.appendChild(productDiv)
-      productDiv.appendChild(imageElement)
-      productDiv.appendChild(descriptionElement)
-      productDiv.appendChild(priceElement)
-      descriptionElement.innerText=product.description;
-      
-  
-  
-      imageElement.setAttribute('src',product.image_link)
-      const price=Math.round((product.price)*100)
-      priceElement.innerText=`Ksh. ${price}`
-      imageElement.className='productImage'
-      priceElement.className='price'
-      productDiv.className='productDiv'
-      descriptionElement.className='description'
-  
-      
-      
-    })
-      
-      
-  
-      
-      
-          
-    })
-  });
-  
-  // })
+      data.forEach((product) => {
+        const parentDiv = document.getElementById("item");
+        const productDiv = document.createElement("div");
+        const titleElement = document.createElement("h6");
+        const imageElement = document.createElement("img");
+        const descriptionElement = document.createElement("p");
+        const priceElement = document.createElement("h5");
+        const priceButton = document.createElement("button");
+        const productLink = product.product_link;
+
+        titleElement.innerText = product.name;
+        productDiv.appendChild(titleElement);
+        parentDiv.appendChild(productDiv);
+        productDiv.appendChild(imageElement);
+        productDiv.appendChild(descriptionElement);
+        productDiv.appendChild(priceElement);
+        descriptionElement.innerText = product.description;
+
+        imageElement.setAttribute("src", product.image_link);
+        const price = Math.round(product.price * 100);
+        priceElement.innerText = `Ksh. ${price}`;
+        imageElement.className = "productImage";
+        priceElement.className = "price";
+        productDiv.className = "productDiv";
+        descriptionElement.className = "description";
+
+        priceElement.addEventListener("click", () => {
+          window.open(productLink);
+        });
+
+        let form = document.getElementById("form");
+        if (form.style.display !== "none") {
+          form.style.display = "flex";
+        } else {
+          form.style.display = "none";
+        }
+      });
+    });
+});
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert(
+    "Thank you for your contribution. Our team will review the product for listing"
+  );
+  setTimeout(() => form.reset(), 3000);
+});
